e099d52c37f6125c6a7439d07b30a2c9af3c51ba
mrremain <thatwildrobloxnoob@gmail.com>
2024-09-10 19:35:43 -0400

    wip, rush only.

---
+++ BigHeadMode.lua
@@ -0,0 +1,14 @@
+function Rush()
+    repeat wait() until workspace.RushMoving
+    if workspace.RushMoving then
+        local rush = workspace.RushMoving
+        local face = rush.RushNew.Attachment.ParticleEmitter
+        local trail = rush.Rushnew.Attachment.BlackTrail
+        face.Texture = "rbxassetid://3031362207"
+        trail.Color = Color3.new(1.000000, 1.000000, 0.000000)
+    end
+end
+
+game:GetService("RunService").RenderStepped:Connect(function()
+    Rush()
+end)
