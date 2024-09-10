6f772618ce07d91a64b56cf0219532742d600105
mrremain <thatwildrobloxnoob@gmail.com>
2024-09-10 19:56:15 -0400

    dieing

--- BigHeadMode.lua
+++ BigHeadMode.lua
@@ -1,16 +1,15 @@
 -- RUSH ONLY, MORE ENTITIES COMING SOON FATTY
 
 function Rush()
-    
-    if workspace.RushMoving then
-        local rush = workspace.RushMoving
-        local face = rush.RushNew.Attachment.ParticleEmitter
-        local trail = rush.Rushnew.Attachment.BlackTrail
-        face.Texture = "rbxassetid://3031362207"
-        trail.Color = Color3.new(1.000000, 1.000000, 0.000000)
-    end
+    local rush = workspace.RushMoving
+    local face = rush.RushNew.Attachment.ParticleEmitter
+    local trail = rush.Rushnew.Attachment.BlackTrail
+    face.Texture = "rbxassetid://3031362207"
+    trail.Color = Color3.new(1.000000, 1.000000, 0.000000)
 end
 
 game:GetService("RunService").RenderStepped:Connect(function()
-    Rush()
+    if workspace:WaitForChild("RushMoving") then
+        Rush()
+    end
 end)
